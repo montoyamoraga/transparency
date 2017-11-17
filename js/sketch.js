0e0141d8accb5c5aa940e9016cdad57a56c812d1
montoyamoraga <montoyamoraga@gmail.com>
2017-11-16 22:37:22 -0500

    retrieve keys

--- js/sketch.js
+++ js/sketch.js
@@ -2,6 +2,7 @@
 
 //store json file
 var info = null;
+var keys = null;
 
 //preload() runs first, once
 function preload() {
@@ -12,12 +13,17 @@ function preload() {
 //setup() runs once, after preload()
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  //test if json file works
-  console.log(info["birth-city"]);
+
+  keys = Object.keys(info)
+
+  for (var i = 0; i < keys.length; i++) {
+    console.log(keys);
+  }
+
 }
+
 
 //draw() runs in a loop, after setup()
 function draw() {
-  background(255, 0, 0);
 
 }
